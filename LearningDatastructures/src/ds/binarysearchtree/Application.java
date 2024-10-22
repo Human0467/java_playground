5f356fad3159e7c55b8454f7588a7dc1bd5da88b
Human0467 <mustchjo@gmail.com>
2024-10-22 16:46:01 +0100

    BST

--- LearningDatastructures/src/ds/binarysearchtree/Application.java
+++ LearningDatastructures/src/ds/binarysearchtree/Application.java
@@ -2,4 +2,19 @@ package LearningDatastructures.src.ds.binarysearchtree;
 
 public class Application {
 
+    public static void main(String[] args) {
+    
+    BST<Integer, String> tree = new BST<Integer, String>();
+    tree.insert(10, "ten");
+    tree.insert(8, "eight");
+    tree.insert(100, "hundred");
+    tree.insert(11, "eleven");
+    tree.insert(-1, "minus one");
+    tree.insert(0, "zero");
+    tree.insert(20, "twenty");
+
+    System.out.println(tree.findMax().key);
+    System.out.println(tree.findMin().key);
+
+    }
 }
--- LearningDatastructures/src/ds/binarysearchtree/BST.java
+++ LearningDatastructures/src/ds/binarysearchtree/BST.java
@@ -1,6 +1,6 @@
 package LearningDatastructures.src.ds.binarysearchtree;
 
-public class BST <K, V>{
+public class BST <K extends Comparable<K>, V>{
 
     private Node<K,V> root;
 
@@ -11,6 +11,47 @@ public class BST <K, V>{
         } else {
             Node<K,V> current = root;
             Node<K,V> parent;
+
+            while(true) {
+                parent = current;
+                if(key.compareTo(current.key) < 0){
+                    current = current.leftChild;
+                    if(current == null){ // it's parent is the leaf node
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                } else {
+                    current = current.rightChild;
+                    if(current == null){ // it's parent is the leaf node
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
         }
+    }
+
+    public Node<K,V> findMin(){
+        Node<K,V> current = root;
+        Node<K,V> last = null;
+
+        while(current != null){
+            last = current;
+            current = current.leftChild;
+        }
+
+        return last;
+    }
+
+    public Node<K,V> findMax(){
+        Node<K,V> current = root;
+        Node<K,V> last = null;
+
+        while(current != null){
+            last = current;
+            current = current.rightChild;
+        }
+
+        return last;
     }
 }
